1a06dafa6d8dd7f6bbf8b1bdd702e66d47902d16
Katherine Tam <katherinemtam@gmail.com>
2021-07-26 17:22:20 -0700

    add h1 and RandomColor container

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -1,5 +1,11 @@
 import React from 'react';
+import RandomColor from '../../containers/RandomColor';
 
 export default function App() {
-  return <h1>Hello World</h1>;
+  return (
+    <>
+      <h1>World of Color!</h1>
+      <RandomColor />
+    </>
+  );
 }
